9eb44080297377b0be2cb2a32399678d2ad404c3
Aabid Mitha <aabidmitha@yahoo.com>
2019-03-19 02:14:10 -0400

    Created main app prototype. By: Aabid Mitha

--- src/com/main/HomePage.java
+++ src/com/main/HomePage.java
@@ -1,4 +1,4 @@
-/*
+
 package com.main;
 
 import javafx.application.Application;
@@ -8,43 +8,39 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
 
-public class TaskAdderApp extends Application{
+
+public class HomePage extends Application{
     
     private TextField nameField, notesField;
     
-    public TaskAdderApp(){
+    public HomePage(){
         nameField = new TextField();
         notesField = new TextField();
     }
     
-    public void start(Stage stage){
+    public void start(Stage primaryStage){
         GridPane pane = new GridPane();
-        
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
         Button okButton = new Button("Create Task");
         okButton.setOnMousePressed(e -> {
-            Task t = createTask();
+            Task t = new Task("Task 1");
             System.out.println(t.toString());
         });
         
-        pane.add(new Label("Enter task name: "), 0, 0);
+        pane.add(new Label("Task name: "), 0, 0);
         pane.add(nameField, 1, 0);
-        pane.add(new Label("Enter task name: "), 0, 1);
+        pane.add(new Label("Due date: "), 0, 1);
         pane.add(notesField, 1, 1);
         pane.add(okButton, 1, 2);
         
-        stage.setScene(new Scene(pane));
-        stage.show();
-    }
-    
-    public Task createTask(){
-        Task t = new Task(nameField.getText());
-        t.setNotes(notesField.getText());
-        return t;
+        Scene scene = new Scene(pane, 500, 150);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
     public static void main(String[] args){
         Application.launch(args);
     }
 }
-*/
