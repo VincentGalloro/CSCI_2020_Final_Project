7486fd6d7d90df083694ca784edc4c2e515884ac
Aabid Mitha <aabidmitha@yahoo.com>
2019-03-19 03:07:05 -0400

    Added settings and add task buttons (images) to main GUI. By: Aabid Mitha

--- src/com/main/HomePage.java
+++ src/com/main/HomePage.java
@@ -7,8 +7,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class HomePage extends Application{
@@ -21,8 +25,12 @@ public class HomePage extends Application{
     }
     
     public void start(Stage primaryStage){
+        BorderPane mainPane = new BorderPane();
+       
+        //Pane for task info
         GridPane pane = new GridPane();
         pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+     
         Button okButton = new Button("Create Task");
         okButton.setOnMousePressed(e -> {
             Task t = new Task("Task 1");
@@ -35,7 +43,24 @@ public class HomePage extends Application{
         pane.add(notesField, 1, 1);
         pane.add(okButton, 1, 2);
         
-        Scene scene = new Scene(pane, 500, 150);
+        //Pane that holds the pane for buttons
+        BorderPane holdButtonsPane = new BorderPane();
+        ImageView settingsButton = new ImageView(new Image("file:///C:/Users/Aabid Mitha/OneDrive/CSCI_2020_Final_Project/CSCI_2020_Final_Project/Images/settingsButton.png"));
+        ImageView addTaskButton = new ImageView(new Image("file:///C:/Users/Aabid Mitha/OneDrive/CSCI_2020_Final_Project/CSCI_2020_Final_Project/Images/addTaskButton.png"));
+        
+        //Pane that has the settings and add new task buttons
+        GridPane buttonsPane = new GridPane(); 
+        buttonsPane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        buttonsPane.setHgap(10);
+        buttonsPane.add(settingsButton, 0, 0);
+        buttonsPane.add(addTaskButton, 1, 0);
+        holdButtonsPane.setRight(buttonsPane);
+        
+        //Add GridPanes to BorderPane
+        mainPane.setRight(pane);
+        mainPane.setBottom(holdButtonsPane);
+        
+        Scene scene = new Scene(mainPane, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
