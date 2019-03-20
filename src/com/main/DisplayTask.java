6dc7e380c4b559a477284c1038faeed95fb8f8c9
PulkitMadan <pulkit.madan@uoit.net>
2019-03-20 15:58:35 -0400

    Added code to get attributes from Task object

--- src/com/main/DisplayTask.java
+++ src/com/main/DisplayTask.java
@@ -1,4 +1,5 @@
 package com.main;
+import java.util.Date;
 import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -14,6 +15,12 @@ import javafx.stage.Stage;
 
 public class DisplayTask extends Application {
 
+    private Task t;
+    private Date todaysDate  = new Date();
+    public DisplayTask(Task task){
+        t = task;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         VBox root = new VBox(10);
@@ -22,31 +29,28 @@ public class DisplayTask extends Application {
         root.prefHeightProperty().bind(primaryStage.heightProperty().multiply(0.8));
         root.setPadding(new Insets(5, 5, 5, 5));
 
-
-        //TODO  method to get task name and due date
-
         //Horizontal Pane to display Task name and due date
         HBox hb1 = new HBox();
         hb1.prefWidthProperty().bind(root.widthProperty());
         hb1.setStyle("-fx-border-color: #000000;-fx-border-radius: 5");
-        Label lTaskName = new Label("This is the name of the task");
+        Label lTaskName = new Label(t.getName());
         lTaskName.prefWidthProperty().bind(root.widthProperty().multiply(0.6));
-        
-        //TODO make the text bold
+
+        //TODO change text style
         Label lDue = new Label("DUE: ");
+        lDue.setFont(Font.font("Times New Roman", FontWeight.BOLD, 14));
         lDue.setAlignment(Pos.CENTER_RIGHT);
         lDue.prefWidthProperty().bind(root.widthProperty().multiply(0.15));
 
-        Label lDueDate = new Label("Due date");
+        Label lDueDate = new Label(String.valueOf(t.getDueDate()));
         lDueDate.prefWidthProperty().bind(root.widthProperty().multiply(0.25));
         hb1.getChildren().addAll(lTaskName,lDue, lDueDate);
 
         //TODO  method to find due time
 
         Label lDueIn = new Label("This task is due in _ days");
-        lDueIn.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD, FontPosture.ITALIC, 15));
-        
-        //TODO make the text bold
+        lDueIn.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD, FontPosture.ITALIC, 16));
+
         Label lNotes = new Label("Notes");
         lNotes.setStyle("-fx-border-color: black;");
         lNotes.setPadding(new Insets(2,2,2,2));
@@ -56,7 +60,7 @@ public class DisplayTask extends Application {
         Label lDisplayNotes = new Label();
         lDisplayNotes.prefWidthProperty().bind(root.widthProperty());
         lDisplayNotes.prefHeightProperty().bind(root.heightProperty().multiply(0.3));
-        lDisplayNotes.setText("this is line 1 \n this is line 2 \n this is line 3\n this is line 4\n this is line 5 ");
+        lDisplayNotes.setText(t.getNotes());
         lDisplayNotes.setAlignment(Pos.TOP_LEFT);
         lDisplayNotes.setStyle("-fx-border-color: #000000; -fx-background-color: rgb(209,193,245)");
 
@@ -67,7 +71,7 @@ public class DisplayTask extends Application {
 
 
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle(t.getName());
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
