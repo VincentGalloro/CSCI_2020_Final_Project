2b680a4729f271a3a02223d45de8dfc86ba48edf
Vincent Galloro <vincent.galloro@uoit.net>
2019-03-23 16:48:27 -0400

    Added a prototype of the Send Task Page, and added a socket to the TaskClient

---
+++ src/com/main/pages/SendTaskPage.java
@@ -0,0 +1,43 @@
+package com.main.pages;
+
+import com.main.Task;
+import com.main.TaskPool;
+import com.main.taskio.TaskClient;
+import java.util.ArrayList;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class SendTaskPage{
+
+    private TextArea area;
+    private TaskClient client;
+    
+    public SendTaskPage(){
+        client = new TaskClient(new ArrayList<Task>(), this::writeLine);
+    }
+    
+    public void start(Stage stage){
+        Pane pane = new Pane();
+        
+        area = new TextArea();
+        area.setFont(Font.font(20));
+        area.setEditable(false);
+        
+        pane.getChildren().add(area);
+        
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+    
+    public void writeLine(String s){
+        Platform.runLater(() -> area.setText(area.getText() + s + "\n"));
+    }
+    
+    /*public static void main(String[] args){
+        Application.launch(args);
+    }*/
+}
--- src/com/main/taskio/TaskClient.java
+++ src/com/main/taskio/TaskClient.java
@@ -1,6 +1,23 @@
 
 package com.main.taskio;
 
-public class TaskClient {
-    //TODO
+import com.main.Task;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class TaskClient implements Runnable{
+    
+    private final ArrayList<Task> tasks;
+    private final Consumer<String> log;
+    
+    public TaskClient(ArrayList<Task> tasks, Consumer<String> log){
+        this.tasks = tasks;
+        this.log = log;
+    }
+
+    public void run() {
+        Socket socket = new Socket();
+        //socket.connect(new SocketAddress("localhost", 8000));
+    }
 }
