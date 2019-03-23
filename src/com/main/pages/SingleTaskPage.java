a8b4684795125181c94219bce365b1d701e98460
PulkitMadan <pulkit.madan@uoit.net>
2019-03-23 00:03:27 -0400

    Changed the nodes to more appropriate types, added functionality to display task priority with color coding

--- src/com/main/pages/SingleTaskPage.java
+++ src/com/main/pages/SingleTaskPage.java
@@ -1,14 +1,21 @@
 package com.main.pages;
 import com.main.Task;
+import com.main.TaskPriority;
+import java.util.ArrayList;
 import java.util.Date;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.text.*;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
 
 public class SingleTaskPage extends Application {
@@ -16,6 +23,7 @@ public class SingleTaskPage extends Application {
     private Task t;
     private Date todaysDate  = new Date();
     private Runnable refresher;
+    private ArrayList<TextField> tfs = new ArrayList<>() ;
     
     public SingleTaskPage(Task task, Runnable refresher){
         t = task;
@@ -32,42 +40,90 @@ public class SingleTaskPage extends Application {
 
         //Horizontal Pane to display Task name and due date
         HBox hb1 = new HBox();
+        hb1.setPadding(new Insets(5, 5, 5, 5));
         hb1.prefWidthProperty().bind(root.widthProperty());
         hb1.setStyle("-fx-border-color: #000000;-fx-border-radius: 5");
-        Label lTaskName = new Label(t.getName());
-        lTaskName.prefWidthProperty().bind(root.widthProperty().multiply(0.6));
+        hb1.setAlignment(Pos.CENTER);
+        
+        //text field to display the name
+        TextField tfTaskName = new TextField(t.getName());
+        tfTaskName.setStyle("-fx-border-color: #ffffff; -fx-background-color : rgb(253,255,226); -fx-opacity: 1.0; -fx-border-radius: 5");
+        tfTaskName.setFont(Font.font("Consolas", FontWeight.SEMI_BOLD, 16));
+        tfTaskName.prefWidthProperty().bind(root.widthProperty().multiply(0.6));
+        tfTaskName.setDisable(true);
+        tfs.add(tfTaskName);
 
-        //TODO change text style
+        //Label to display the due date
         Label lDue = new Label("DUE: ");
-        lDue.setFont(Font.font("Times New Roman", FontWeight.BOLD, 14));
-        lDue.setAlignment(Pos.CENTER_RIGHT);
+        lDue.setFont(Font.font("Consolas", FontWeight.BOLD, 16));
+        lDue.setAlignment(Pos.BOTTOM_RIGHT);
+        lDue.setStyle("-fx-border-color: #ffffff");
         lDue.prefWidthProperty().bind(root.widthProperty().multiply(0.15));
 
-        Label lDueDate = new Label(String.valueOf(t.getDueDate()));
-        lDueDate.prefWidthProperty().bind(root.widthProperty().multiply(0.25));
-        hb1.getChildren().addAll(lTaskName,lDue, lDueDate);
+        //textField to display the due date
+        TextField tfDueDate = new TextField(String.valueOf(t.getDueDate()));
+        tfDueDate.setFont(Font.font("Consolas", FontWeight.SEMI_BOLD, 16));
+        tfDueDate.setStyle("-fx-border-color: #ffffff; -fx-background-color : rgb(253,255,226); -fx-opacity: 1.0; -fx-border-radius: 5");
+        tfDueDate.prefWidthProperty().bind(root.widthProperty().multiply(0.25));
+        tfDueDate.setDisable(true);
+        tfs.add(tfDueDate);
+        hb1.getChildren().addAll(tfTaskName,lDue, tfDueDate);
+ 
+    
+        //Button to display the priority of task
+        Button bPriority = new Button();
+        bPriority.setPadding(new Insets(5, 5, 5, 5));
+        //color coding
+        if(t.getPriority()==TaskPriority.HIGH){
+            bPriority.setText("This task is of HIGH priority");
+            bPriority.setTextFill(Color.RED);
+        }
+        if(t.getPriority()==TaskPriority.MEDIUM){
+            bPriority.setText("This task is of MEDIUM priority");
+            bPriority.setTextFill(Color.BLUE);
+        }
+        if(t.getPriority()==TaskPriority.LOW){
+            bPriority.setText("This task is of LOW priority");
+            bPriority.setTextFill(Color.GREEN);
+        }
+                
+        bPriority.setStyle("-fx-border-color: rgb(253,255,226); -fx-background-color : rgb(253,255,226); -fx-opacity: 1.0; -fx-border-radius: 5");
+        bPriority.setFont(Font.font("Consolas", FontWeight.SEMI_BOLD, 16));
+        
+        //adding edit(pencil) imagee to the button
+        ImageView IeditDueIn = new ImageView("file:images/pencil2.png");
+        IeditDueIn.setFitWidth(20);
+        IeditDueIn.setFitHeight(bPriority.getHeight());
+        IeditDueIn.setPreserveRatio(true);
+        Button bEditDueIn = new Button();
+        bEditDueIn.setGraphic(IeditDueIn);
+        bEditDueIn.setStyle("-fx-background-color : rgb(253,255,226); -fx-border-color: #000000");
+        HBox hb2 = new HBox(bPriority, bEditDueIn);
+        hb2.setPadding(new Insets(5, 5, 5, 5));
+        hb2.setSpacing(7);
 
-        //TODO  method to find due time
-
-        Label lDueIn = new Label("This task is due in _ days");
-        lDueIn.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD, FontPosture.ITALIC, 16));
-
+        
+        
         Label lNotes = new Label("Notes");
+        lNotes.setFont(Font.font("Consolas", FontWeight.BOLD, 17));
+        lNotes.setPadding(new Insets(5,5,5,5));
         lNotes.setStyle("-fx-border-color: black;");
-        lNotes.setPadding(new Insets(2,2,2,2));
         lNotes.setStyle("-fx-border-color: #000000;-fx-border-radius: 7");
 
         //TODO -method to get text
-        Label lDisplayNotes = new Label();
-        lDisplayNotes.prefWidthProperty().bind(root.widthProperty());
-        lDisplayNotes.prefHeightProperty().bind(root.heightProperty().multiply(0.3));
-        lDisplayNotes.setText(t.getNotes());
-        lDisplayNotes.setAlignment(Pos.TOP_LEFT);
-        lDisplayNotes.setStyle("-fx-border-color: #000000; -fx-background-color: rgb(209,193,245)");
+        TextField tfDisplayNotes = new TextField();
+        tfDisplayNotes.prefWidthProperty().bind(root.widthProperty());
+        tfDisplayNotes.prefHeightProperty().bind(root.heightProperty().multiply(0.3));
+        tfDisplayNotes.setText(t.getNotes());
+        tfDisplayNotes.setAlignment(Pos.TOP_LEFT);
+        tfDisplayNotes.setStyle("-fx-border-color: #000000; -fx-background-color: rgb(209,193,245);  -fx-opacity: 1.0");
+        tfDisplayNotes.setDisable(true);
+        tfs.add(tfDisplayNotes);
+        
 
         //refresher.run();
 
-        root.getChildren().addAll(hb1, lDueIn, lNotes, lDisplayNotes);
+        root.getChildren().addAll(hb1, hb2, lNotes, tfDisplayNotes);
 
 
 
