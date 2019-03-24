c62705df2380e204cc4b50e1b36a2b22d16447c2
Aabid Mitha <aabid.mitha@uoit.net>
2019-03-24 16:02:14 -0400

    Added buttons and functionality for completed and date filters. By: Aabid Mitha

--- src/com/main/pages/SettingsPage.java
+++ src/com/main/pages/SettingsPage.java
@@ -2,8 +2,11 @@ package com.main.pages;
 
 import com.main.TaskPoolQuery;
 import com.main.TaskPriority;
+import com.main.filters.CompletedFilter;
+import com.main.filters.DateFilter;
 import com.main.filters.Filter;
 import com.main.filters.PriorityFilter;
+import java.util.Date;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -32,24 +35,50 @@ public class SettingsPage {
         l.setStyle("-fx-font-weight: bold; -fx-font-size: 20");
         filtersPane.add(l, 0, 0);
 
+        //Set filter buttons
         Button btHighPriority = new Button("High Priority");
-        Button btMediumPrioirty = new Button("Medium Priority");
+        btHighPriority.setPrefWidth(225);
+        Button btMediumPriority = new Button("Medium Priority");
+        btMediumPriority.setPrefWidth(225);
         Button btLowPriority= new Button("Low Priority");
-        filtersPane.add(btHighPriority, 0, 1);
-        filtersPane.add(btMediumPrioirty, 1, 1);
-        filtersPane.add(btLowPriority, 2, 1);
+        btLowPriority.setPrefWidth(225);
+        Button btCompleted = new Button("Completed");
+        btCompleted.setPrefWidth(225);
+        Button btUncompleted= new Button("Uncompleted");
+        btUncompleted.setPrefWidth(225);
+        Button btDate= new Button("Date");
+        btDate.setPrefWidth(225);
         
+
+        filtersPane.add(btHighPriority, 0, 1);
+        filtersPane.add(btMediumPriority, 1, 1);
+        filtersPane.add(btLowPriority, 2, 1);
+        filtersPane.add(btCompleted, 0, 2);
+        filtersPane.add(btUncompleted, 1, 2);
+        filtersPane.add(btDate, 2, 2);
+
+        
+        //Set functionality for each filter
         btLowPriority.setOnMousePressed(e ->{
             applyFilter(new PriorityFilter(TaskPriority.LOW));
         });
-        btMediumPrioirty.setOnMousePressed(e ->{
+        btMediumPriority.setOnMousePressed(e ->{
             applyFilter(new PriorityFilter(TaskPriority.MEDIUM));
         });        
         btHighPriority.setOnMousePressed(e ->{
             applyFilter(new PriorityFilter(TaskPriority.HIGH));
         });
+        btCompleted.setOnMousePressed(e ->{
+            applyFilter(new CompletedFilter(true));
+        });
+        btUncompleted.setOnMousePressed(e ->{
+            applyFilter(new CompletedFilter(false));
+        });
+        btDate.setOnMousePressed(e ->{
+            applyFilter(new DateFilter(new Date()));
+        });
         
-        Scene scene = new Scene(filtersPane, 370, 100);
+        Scene scene = new Scene(filtersPane, 500, 150);
         stage.setScene(scene);
         stage.show();
     }
